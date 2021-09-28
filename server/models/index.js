fdf6a85e7ead97154dffdad4740b2f6501b58cb4
Browntuckerr <82062612+Browntuckerr@users.noreply.github.com>
2021-09-28 19:13:46 -0400

    fixed server start up

--- server/models/index.js
+++ server/models/index.js
@@ -1,3 +1,5 @@
+const Dungeon = require("./dungeons");
+const Characters = require("./characters");
 const User = require('./user');
+module.exports = { User, Characters, Dungeon };
 
-module.exports = { User };
